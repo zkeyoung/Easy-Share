7f99b42ac64ad48f8958e39dc9c13927f2f6e386
zhangky <iszhangky@163.com>
2023-03-09 14:23:44 +0800

    feat: add notification

--- src/lib/server/router.ts
+++ src/lib/server/router.ts
@@ -1,5 +1,8 @@
+import { Notification } from 'electron';
 import express, { Request, Response } from 'express';
+import { CacheKey, Language } from '../../constant';
 import File from '../../entity/file';
+import Settings from '../../entity/settings';
 import { cache } from '../cache';
 
 const router = express.Router();
@@ -9,6 +12,15 @@ router.get('/:fileId', async (req: Request, res: Response) => {
   try {
     const file: File = cache.get(fileId);
     if (!file) return res.sendStatus(404);
+
+    if (Notification.isSupported()) {
+      const { language }: Settings = cache.get(CacheKey.SETTINGS);
+      new Notification({
+        title: 'Easy Share',
+        body: `${file.name} ${language === Language.enUS ? 'is being downloaded...' : '正在被下载...'}`,
+      }).show();;
+    }
+
     res.download(file.path, file.name, (err) => {
       if (err && !res.headersSent) res.json(`${file.name} download failed`);
     });
